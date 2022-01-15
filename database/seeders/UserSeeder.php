89de05d2277d1baf5b5df64789cd43ddbe1d9cbe
Lacielico <marwinsious@gmail.com>
2022-01-15 11:48:16 +0700

    seeder + nama db

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,7 +11,10 @@ class UserSeeder extends Seeder
     public function run()
     {
         $this->createUser('admin', 'Fajar', 'fajar.hamdani@binus.ac.id', '6287786552759', 'Jakarta, Indonesia', 'li01laravel');
-        $this->createUser('member', 'Member 1', 'member1@yopmail.com', '6287786552759', 'Palembang, Indonesia', 'li01laravel');
+        $this->createUser('member', 'Marwin', 'marwin@mailnesia.com', '6287786552759', 'Palembang, Indonesia', 'li01laravel');
+        $this->createUser('member', 'Marcelino', 'marcelino@yopmail.com', '6289578836009', 'Bandung, Indonesia', 'li01laravel');
+        $this->createUser('member', 'Gabriel', 'gabriel@gmail.com', '6287968900435', 'Surabaya, Indonesia', 'li01laravel');
+
     }
 
     private function createUser($role, $name, $email, $phone, $address, $password)
